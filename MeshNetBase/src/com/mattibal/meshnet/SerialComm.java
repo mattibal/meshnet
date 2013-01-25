ee649b3d2b288df9d1645fe73a814f0249c511c9
mattibal <mattibal@gmail.com>
2013-01-25 04:35:08 +0100

    fatto l'hdlc... se funziona è un miracolo!!

--- MeshNetBase/src/com/mattibal/meshnet/SerialComm.java
+++ MeshNetBase/src/com/mattibal/meshnet/SerialComm.java
@@ -19,18 +19,24 @@ import java.util.TooManyListenersException;
  * SerialPortEventListener to avoid polling.
  * 
  */
-public class SerialComm {
+public class SerialComm implements SerialPortEventListener{
 	
 	public static final int TIME_OUT = 2000;
 	
+	protected InputStream inStream;
 	protected OutputStream outStream;
+	
+	private byte[] readBuffer = new byte[1024];
+	
+	protected SerialLayer2 layer2;
+	
 	
 	public SerialComm(String portName) throws NoSuchPortException, PortInUseException, UnsupportedCommOperationException, IOException, TooManyListenersException{
 		
 		CommPortIdentifier portIdentifier = CommPortIdentifier
 				.getPortIdentifier(portName);
 		if (portIdentifier.isCurrentlyOwned()) {
-			System.out.println("Error: Port is currently in use");
+			throw new IOException("Port is currently in use");
 		} else {
 			CommPort commPort = portIdentifier.open(this.getClass().getName(),
 					TIME_OUT);
@@ -40,51 +46,48 @@ public class SerialComm {
 				serialPort.setSerialPortParams(9600, SerialPort.DATABITS_8,
 						SerialPort.STOPBITS_1, SerialPort.PARITY_NONE);
 
-				InputStream in = serialPort.getInputStream();
+				inStream = serialPort.getInputStream();
 				outStream = serialPort.getOutputStream();
 
 				//(new Thread(new SerialWriter(out))).start();
 
-				serialPort.addEventListener(new SerialReader(in));
+				serialPort.addEventListener(this);
 				serialPort.notifyOnDataAvailable(true);
 
 			} else {
-				throw new IOException("Only serial ports are handled by this example.");
+				throw new IOException("This is not a serial port!.");
 			}
 		}
-	}
-
-	/**
-	 * Handles the input coming from the serial port. A new line character is
-	 * treated as the end of a block in this example.
-	 */
-	public static class SerialReader implements SerialPortEventListener {
 		
-		private InputStream in;
-		private byte[] buffer = new byte[1024];
-
-		public SerialReader(InputStream in) {
-			this.in = in;
-		}
-
-		public void serialEvent(SerialPortEvent arg0) {
-			int data;
-
-			try {
-				int len = 0;
-				while ((data = in.read()) > -1) {
-					if (data == '\n') {
-						break;
-					}
-					buffer[len++] = (byte) data;
-				}
-				System.out.print(new String(buffer, 0, len));
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
+		this.layer2 = new SerialLayer2(this);
 	}
+
+	
+
+	@Override
+	public void serialEvent(SerialPortEvent arg0) {
+				
+		int data;
+
+		try {
+			int len = 0;
+			while ((data = inStream.read()) > -1) {
+				layer2.onSerialByteReceived(data);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	/**
+	 * Send via serial port the byte contained in the int parameter
+	 */
+	public void transmitByte(int dataByte){
+		// TODO
+	}
+	
 
 	
 	/*public static class SerialWriter implements Runnable {
